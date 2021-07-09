2b8b1833491d4497558c681fb798f73aec970e5e
rkurian <kurian.rohit1229@gmail.com>
2021-07-09 17:29:51 -0500

    made typedefs and resolvers files

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -6,35 +6,16 @@ import Auth from '../utils/auth';
 import { removeBookId } from '../utils/localStorage';
 
 const SavedBooks = () => {
-  const [userData, setUserData] = useState({});
+  //const [userData, setUserData] = useState({});
 
   // use this to determine if `useEffect()` hook needs to run again
   const userDataLength = Object.keys(userData).length;
 
-  useEffect(() => {
-    const getUserData = async () => {
-      try {
-        const token = Auth.loggedIn() ? Auth.getToken() : null;
+  const { loading, userData } = useQuery( QUERY_ME, {
+    variables: { username: userParam }
+  });
 
-        if (!token) {
-          return false;
-        }
 
-        const response = await getMe(token);
-
-        if (!response.ok) {
-          throw new Error('something went wrong!');
-        }
-
-        const user = await response.json();
-        setUserData(user);
-      } catch (err) {
-        console.error(err);
-      }
-    };
-
-    getUserData();
-  }, [userDataLength]);
 
   // create function that accepts the book's mongo _id value as param and deletes the book from the database
   const handleDeleteBook = async (bookId) => {
---
+++ server/schemas/resolvers.js
@@ -0,0 +1,78 @@
+const { AuthenticationError } = require('apollo-server-express');
+const { User, Book } = require('../models');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+    Query: {
+        me: async (parent, args, context) => {
+            if (context.user) {
+                const userData = await User.findOne({ _id: context.user._id })
+                    .select('-__v -password')
+                    .populate('savedBooks')
+
+                return userData;
+            }
+
+            throw new AuthenticationError('Not logged in');
+        }
+    },
+    Mutation: {
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+
+            if (!user) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
+
+            const correctPw = await user.isCorrectPassword(password);
+
+            if (!correctPw) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
+
+            const token = signToken(user);
+            return { token, user };
+        },
+        addUser: async (parent, args) => {
+            const user = await User.create(args);
+            const token = signToken(user);
+
+            return { token, user };
+        },
+        saveBook: async (parent, { authors, description, title, image, link }) => {
+            const user = await User.findOneAndUpdate(
+                { _id: context.user._id },
+                {
+                    $push: {
+                        savedBooks: {
+                            authors,
+                            description,
+                            title,
+                            image,
+                            link
+                        }
+                    }
+                }, {
+                new: true
+            });
+            return user
+
+        },
+        removeBook: async (parent, { bookId }) => {
+            const user = await User.findOneAndUpdate({ _id: context.user._id },
+                {
+                    $pull: {
+                        savedBooks: {
+                            bookId
+                        }
+                    }
+                }, {
+                new: true
+            });
+            return user
+
+        }
+    }
+}
+
+module.exports = resolvers;
